262c6d2866308e3def1b7b23138a7e7e4bebf42b
godxiaocui <61813963+godxiaocui@users.noreply.github.com>
2023-04-22 20:58:38 +0800

    main

---
+++ springboot_05_mybatis/src/test/java/com/czh/springboot/springboot_05_mybatis/Springboot05MybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czh.springboot.springboot_05_mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot05MybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
