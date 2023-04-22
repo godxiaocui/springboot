0ae5686a13120116db9ee8858271f695b9c1ec3f
godxiaocui <godxiaocui@gmail.com>
2023-04-22 21:15:36 +0800

    first commit

---
+++ springboot_05_mybatis/src/main/java/com/czh/springboot/springboot_05_mybatis/dao/BookDao.java
@@ -0,0 +1,12 @@
+package com.czh.springboot.springboot_05_mybatis.dao;
+
+import com.czh.springboot.springboot_05_mybatis.pojo.Book;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface BookDao {
+    @Select("select * from tbl_book where id = #{id}")
+    Book getById(@Param("id") Integer id);
+}
--- springboot_05_mybatis/src/test/java/com/czh/springboot/springboot_05_mybatis/Springboot05MybatisApplicationTests.java
+++ springboot_05_mybatis/src/test/java/com/czh/springboot/springboot_05_mybatis/Springboot05MybatisApplicationTests.java
@@ -1,13 +1,20 @@
 package com.czh.springboot.springboot_05_mybatis;
 
+import com.czh.springboot.springboot_05_mybatis.dao.BookDao;
+import com.czh.springboot.springboot_05_mybatis.pojo.Book;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Springboot05MybatisApplicationTests {
+	@Autowired
+	private BookDao bookDao;
 
 	@Test
 	void contextLoads() {
+		Book byId = bookDao.getById(1);
+		System.out.println(byId);
 	}
 
 }
